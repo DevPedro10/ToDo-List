1376e768bec6c567ad9b2b67359773b5ac1b3f34
Natã <natansilva@plusoft.com>
2023-03-27 21:39:53 -0300

    update 0

--- To Do List/main.js
+++ To Do List/main.js
@@ -1,30 +1,70 @@
-const btnCreateTask = document.querySelector("#createTask");
-const listDiv = document.querySelector("#listDiv");
-let conteudo = document.querySelector("#conteudo");
+let inputTarefa = document.querySelector("#conteudo");
+const botaoCriaTarefa = document.querySelector("#createTask");
+let retornoDiv = document.querySelector("#listDiv");
 
-let ulTag = document.createElement("ul");
 
-btnCreateTask.addEventListener("click", (event) => {
-    let liTag = document.createElement("li");
-    liTag.textContent = conteudo.value;
-    let btnDeleteTask = document.createElement("button");
-    btnDeleteTask.textContent = "Delete"
-    liTag.appendChild(btnDeleteTask)
-    ulTag.appendChild(liTag)
-    listDiv.appendChild(ulTag)
 
-    localStorage.setItem("lista", listDiv)
+
+function createItemList(value) {
+    let li = document.createElement("li");
+    li.textContent = value;
+    li.appendChild(createDeleteButton());
+    return li;
+}
+
+function createTask(element) {
+let showValue = createItemList(element);    
+retornoDiv.appendChild(showValue);
+}
+
+function createDeleteButton() {
+    const deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("class", "deleteBtn");
+    deleteBtn.textContent = "Apagar";
+    return deleteBtn;
+}
+
+botaoCriaTarefa.addEventListener("click", createTask);
+document.addEventListener("keypress", (e) => {
+    if(e.key == 'Enter') {
+        createTask(inputTarefa.value);
+        inputTarefa.value = '';
+        guardaTarefas()
+
+    }
+})
+
+document.addEventListener("click", (e) => {
+    const element = e.target
+    if(element.classList.contains("deleteBtn")) {
+        element.parentElement.remove();
+        guardaTarefas()
+    }
+})
+
+
+function guardaTarefas() {
+    let itensLista = new Array()
+    const tarefas = retornoDiv.querySelectorAll("li");
+    for(let tarefa of tarefas) {
+        tarefa = tarefa.innerText.replace('Apagar', ' ');
+        itensLista.push(tarefa);
+    }
+    let parseString = JSON.stringify(itensLista);
+    localStorage.setItem("tarefa", parseString)
+    console.log(parseString)
+    return parseString;
+}
+
+function adicionaTarefasSalvar() {
+    const pegaTarefa = localStorage.getItem("tarefa");
+    const parseArray = JSON.parse(pegaTarefa);
     
-    
-    btnDeleteTask.addEventListener("click", () => {
-        liTag.style.display = "none"
-    });
-});
+    for(let tarefa of parseArray) {
+        createTask(tarefa)
+        // console.log(tarefa)
+    }
 
-window.addEventListener("load", ()=> {
-    const getDataInWebBrowser = localStorage.getItem("lista")
+}
 
-
-});
-
-
+console.log(adicionaTarefasSalvar())
